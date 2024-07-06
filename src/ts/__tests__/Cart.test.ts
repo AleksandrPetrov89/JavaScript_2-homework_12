f68dcba8753eadbf46aa5ed4402b97051e0f181d
AleksandrPetrov89 <petrovav89@gmail.com>
2024-07-06 18:09:50 +0500

    Made a class "Movie" with tests

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,35 @@
 import Cart from '../service/Cart';
+import Movie from '../domain/Movie';
+
+const cart = new Cart();
+const item1 = {
+  id: 1,
+  name: 'name',
+  price: 100
+};
 
 test('new card should be empty', () => {
-  const cart = new Cart();
-
   expect(cart.items.length).toBe(0);
+});
+
+test('Проверка метода add()', () => {
+  cart.add(item1);
+  expect(cart.items.length).toBe(1);
+});
+
+const item2 = new Movie(
+  2,
+  'Мстители',
+  150,
+  2012,
+  'США',
+  'Avengers Assemble!',
+  ['фантастика', 'боевик', 'фэнтези', 'приключения'],
+  137,
+  'ссылка на картинку',
+)
+
+test('Проверка добавления класса Movie', () => {
+  cart.add(item2);
+  expect(cart.items.length).toBe(2);
 });
